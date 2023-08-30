a33e76b732e54f0f05891145354589054d48c20c
Paul ALNET <alnet@insa-toulouse.fr>
2023-08-30 16:48:02 +0200

    feat(1-wc): implement wordcount

--- 1-wc/wc.cc
+++ 1-wc/wc.cc
@@ -1,17 +1,50 @@
 #include <fstream>
 #include <string>
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
+// const string file{"lorem.txt"};
+const string file{"empty.txt"};
+// const string file{"wc.cc"};
+
 int main() {
-  ifstream in{"wc.cc"};
+  ifstream in{file};
   string word{};
   int count{0};
+  int totalWordLength{0};
+  string shortestWord{""};
+  string longestWord{""};
 
-  while(in >> word) {
-    cout << word;
+  while (in >> word) {
+    // cout << word;
     count++;
+    totalWordLength += word.size();
+    if (word.size() < shortestWord.size() || shortestWord.size() == 0)
+      shortestWord = word;
+    longestWord = word.size() > longestWord.size() ? word : longestWord;
   }
-  return 0;
+
+  if (count == 0) {
+    cout << "WARNING: file is empty" << endl;
+    return 1;
+  } else {
+    cout << "There are " << count << " words in the file." << endl;
+
+    cout << "The longest word was \"" << longestWord << "\" with ";
+    cout << longestWord.size() << " character(s)." << endl;
+
+    cout << "The shortest word was \"" << shortestWord << "\" with ";
+    cout << shortestWord.size() << " character(s)." << endl;
+
+    // Ensure average is a double
+    const double average = static_cast<double>(totalWordLength) / count;
+    // Ensure displayed average has 2 decimals
+    cout << fixed << setprecision(2);
+    cout << "The average length was " << average << " character(s)." << endl;
+
+
+    return 0;
+  }
 }

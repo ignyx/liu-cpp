7765363eb6c45c97ccdfa5c876a073d00519eee3
Paul ALNET <alnet@insa-toulouse.fr>
2023-09-13 15:02:10 +0200

    chore(2-time): use snake case instead of camel case

--- 2-time/Time.cc
+++ 2-time/Time.cc
@@ -5,136 +5,136 @@
 
 using namespace std;
 
-Time operator+(const Time &givenTime1, const int &t) {
-  Time newTime;
-  int seconds = (((givenTime1.seconds + givenTime1.minutes * 60 +
-                   givenTime1.hours * 3600 + t) %
+Time operator+(const Time &given_time_1, const int &t) {
+  Time new_time;
+  int seconds = (((given_time_1.seconds + given_time_1.minutes * 60 +
+                   given_time_1.hours * 3600 + t) %
                   86400) +
                  86400) %
                 86400;
-  newTime.hours = seconds / 3600;
+  new_time.hours = seconds / 3600;
   seconds = seconds % 3600;
-  newTime.minutes = seconds / 60;
+  new_time.minutes = seconds / 60;
   seconds = seconds % 60;
-  newTime.seconds = seconds;
-  return newTime;
+  new_time.seconds = seconds;
+  return new_time;
 }
 
-Time operator+(const Time &givenTime1, const Time &givenTime2) {
-  Time newTime;
-  newTime = givenTime1 + (givenTime2.seconds + givenTime2.minutes * 60 +
-                          givenTime2.hours * 3600);
-  return newTime;
+Time operator+(const Time &given_time_1, const Time &given_time_2) {
+  Time new_time;
+  new_time = given_time_1 + (given_time_2.seconds + given_time_2.minutes * 60 +
+                             given_time_2.hours * 3600);
+  return new_time;
 }
 
-Time operator-(const Time &givenTime1, const int &t) {
-  return givenTime1 + (-t);
+Time operator-(const Time &given_time_1, const int &t) {
+  return given_time_1 + (-t);
 }
 
-Time &operator++(Time &givenTime) {
-  givenTime = operator+(givenTime, 1);
-  return givenTime;
+Time &operator++(Time &given_time) {
+  given_time = operator+(given_time, 1);
+  return given_time;
 }
 
-Time operator++(Time &givenTime, int) {
-  Time tmp{givenTime};
-  ++givenTime;
+Time operator++(Time &given_time, int) {
+  Time tmp{given_time};
+  ++given_time;
   return tmp;
 }
 
-Time &operator--(Time &givenTime) {
-  givenTime = givenTime - 1;
-  return givenTime;
+Time &operator--(Time &given_time) {
+  given_time = given_time - 1;
+  return given_time;
 }
 
-Time operator--(Time &givenTime, int) {
-  Time tmp{givenTime};
-  --givenTime;
+Time operator--(Time &given_time, int) {
+  Time tmp{given_time};
+  --given_time;
   return tmp;
 }
 
-bool operator==(const Time &givenTime1, const Time &givenTime2) {
-  return ((givenTime1.hours == givenTime2.hours) and
-          (givenTime1.minutes == givenTime2.minutes) and
-          (givenTime1.seconds == givenTime2.seconds));
+bool operator==(const Time &given_time_1, const Time &given_time_2) {
+  return ((given_time_1.hours == given_time_2.hours) and
+          (given_time_1.minutes == given_time_2.minutes) and
+          (given_time_1.seconds == given_time_2.seconds));
 }
 
-bool operator!=(const Time &givenTime1, const Time &givenTime2) {
-  return !(givenTime1 == givenTime2);
+bool operator!=(const Time &given_time_1, const Time &given_time_2) {
+  return !(given_time_1 == given_time_2);
 }
 
-bool operator<(const Time &givenTime1, const Time &givenTime2) {
-  if (givenTime1.hours < givenTime2.hours) {
+bool operator<(const Time &given_time_1, const Time &given_time_2) {
+  if (given_time_1.hours < given_time_2.hours) {
     return true;
-  } else if (givenTime1.hours == givenTime2.hours) {
-    if (givenTime1.minutes < givenTime2.minutes) {
+  } else if (given_time_1.hours == given_time_2.hours) {
+    if (given_time_1.minutes < given_time_2.minutes) {
       return true;
-    } else if (givenTime1.minutes == givenTime2.minutes) {
-      return givenTime1.seconds < givenTime2.seconds;
+    } else if (given_time_1.minutes == given_time_2.minutes) {
+      return given_time_1.seconds < given_time_2.seconds;
     }
   }
 
   return false;
 }
 
-bool operator<=(const Time &givenTime1, const Time &givenTime2) {
-  return (givenTime1 < givenTime2) or (givenTime1 == givenTime2);
+bool operator<=(const Time &given_time_1, const Time &given_time_2) {
+  return (given_time_1 < given_time_2) or (given_time_1 == given_time_2);
 }
 
-bool operator>(const Time &givenTime1, const Time &givenTime2) {
-  return !(givenTime1 <= givenTime2);
+bool operator>(const Time &given_time_1, const Time &given_time_2) {
+  return !(given_time_1 <= given_time_2);
 }
 
-bool operator>=(const Time &givenTime1, const Time &givenTime2) {
-  return !(givenTime1 < givenTime2);
+bool operator>=(const Time &given_time_1, const Time &given_time_2) {
+  return !(given_time_1 < given_time_2);
 }
 
-bool is_valid(Time const &givenTime) {
-  return (givenTime.hours >= 0 and givenTime.hours < 24) and
-         (givenTime.minutes >= 0 and givenTime.minutes < 60) and
-         (givenTime.seconds >= 0 and givenTime.seconds < 60);
+bool is_valid(Time const &given_time) {
+  return (given_time.hours >= 0 and given_time.hours < 24) and
+         (given_time.minutes >= 0 and given_time.minutes < 60) and
+         (given_time.seconds >= 0 and given_time.seconds < 60);
 }
 
-bool is_am(Time const &givenTime) {
-  if (givenTime.hours >= 12) {
+bool is_am(Time const &given_time) {
+  if (given_time.hours >= 12) {
     return false;
   }
   return true;
 }
 
 string fill_two_decimals(int decimal) {
-  string decimalString = to_string(decimal);
-  if (decimalString.length() >= 2) {
-    return decimalString;
+  string decimal_string = to_string(decimal);
+  if (decimal_string.length() >= 2) {
+    return decimal_string;
   } else {
-    decimalString = "0" + decimalString;
-    return decimalString;
+    decimal_string = "0" + decimal_string;
+    return decimal_string;
   }
 }
 
-string to_string(Time const &givenTime, format timeFormat) {
-  string timeString;
-  if (timeFormat == USA) {
-    if (is_am(givenTime)) {
-      timeString = fill_two_decimals((givenTime.hours + 11) % 12 + 1) + ":" +
-                   fill_two_decimals(givenTime.minutes) + ":" +
-                   fill_two_decimals(givenTime.seconds) + " AM";
+string to_string(Time const &given_time, format time_format) {
+  string time_string;
+  if (time_format == USA) {
+    if (is_am(given_time)) {
+      time_string = fill_two_decimals((given_time.hours + 11) % 12 + 1) + ":" +
+                    fill_two_decimals(given_time.minutes) + ":" +
+                    fill_two_decimals(given_time.seconds) + " AM";
     } else {
-      timeString = fill_two_decimals((givenTime.hours + 11) % 12 + 1) + ":" +
-                   fill_two_decimals(givenTime.minutes) + ":" +
-                   fill_two_decimals(givenTime.seconds) + " PM";
+      time_string = fill_two_decimals((given_time.hours + 11) % 12 + 1) + ":" +
+                    fill_two_decimals(given_time.minutes) + ":" +
+                    fill_two_decimals(given_time.seconds) + " PM";
     }
 
   } else {
-    timeString = fill_two_decimals(givenTime.hours) + ":" +
-                 fill_two_decimals(givenTime.minutes) + ":" +
-                 fill_two_decimals(givenTime.seconds);
+    time_string = fill_two_decimals(given_time.hours) + ":" +
+                  fill_two_decimals(given_time.minutes) + ":" +
+                  fill_two_decimals(given_time.seconds);
   }
 
-  return timeString;
+  return time_string;
 }
 
-ostream &operator<<(ostream &os, Time const &givenTime) {
-  os << to_string(givenTime, EUROPEAN) << endl;
+ostream &operator<<(ostream &os, Time const &given_time) {
+  os << to_string(given_time, EUROPEAN) << endl;
   return os;
 }

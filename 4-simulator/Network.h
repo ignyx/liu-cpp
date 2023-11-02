b2c8c85559b6328b33fac296db68b8d5bfbde26e
Paul ALNET <alnet@insa-toulouse.fr>
2023-11-02 14:30:53 +0100

    spec(4-network): corrections

--- 4-simulator/Network.h
+++ 4-simulator/Network.h
@@ -4,9 +4,8 @@
 #include <string>
 #include <vector>
 
-class Connection {
-public:
-  double potential;
+struct Connection {
+  double potential = 0;
 };
 
 class Component {
@@ -16,10 +15,10 @@ public:
   double const get_voltage();
   virtual double const get_current();
   virtual void run_tick();
-  std::vector<Connection> const &get_terminals();
   std::string const &get_name();
 
 private:
+  std::string name;
   Connection &terminal_a;
   Connection &terminal_b;
 };
@@ -28,17 +27,17 @@ class Network {
 public:
   void simulate(unsigned int interations, unsigned int lines, double time_step);
   std::vector<Component> const &get_components();
-  void add_component(Component component);
+  void add_component(Component &component);
   // deallocate ?
 private:
   std::vector<Component> components;
+  // connections ?
 };
 
 class Battery : public Component {
 public:
   Battery(std::string const &name, double voltage, Connection terminal_a,
           Connection terminal_b);
-  double const get_current();
   void run_tick();
 
 private:

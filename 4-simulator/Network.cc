29a75fa4f5f5c0523f96851d8e02fec791b07841
Paul ALNET <alnet@insa-toulouse.fr>
2023-11-02 15:07:35 +0100

    chore(4-network): lint Network.cc

--- 4-simulator/Network.cc
+++ 4-simulator/Network.cc
@@ -1,46 +1,36 @@
-#include <initializer_list>
-#include <iostream> 
-#include <string> 
-#include <iomanip>
 #include "Network.h"
+#include <initializer_list>
+#include <iomanip>
+#include <iostream>
+#include <string>
 
 using namespace std;
- 
- 
- double const Component::get_voltage(){
-    return *terminal_b.potential-*terminal_a.potential;
- }
 
- std::string const Component::&get_name(){
-    return &name;
- }
-
-
-void add_component(Component &component){
-    push_back(component);
-};
-
-double const Component::get_current(){
-    return 0;
+double const Component::get_voltage() {
+  return *terminal_b.potential - *terminal_a.potential;
 }
 
-double const Resistor::get_current(){
-    return get_voltage()/resistance;
+std::string const Component::&get_name() { return &name; }
+
+void add_component(Component &component) { push_back(component); };
+
+double const Component::get_current() { return 0; }
+
+double const Resistor::get_current() { return get_voltage() / resistance; }
+
+double const Capacitor::get_current() {
+  return capacitance * (get_voltage() - stored_charge);
 }
 
-double const Capacitor::get_current(){
-    return capacitance*(get_voltage()-stored_charge);
+void Capacitor::run_step(const double time) {
+  double charge_difference(capacitance * get_voltage() * time);
+  stored_charge = stored_charge + charge_difference;
+  *terminal_a.potential = *terminal_a.potential + charge_difference;
+  *terminal_b.potential = *terminal_b.potential - charge_difference;
 }
 
-void Capacitor::run_step(const double time){
-    double charge_difference(capacitance*get_voltage()*time);
-    stored_charge = stored_charge + charge_difference;
-    *terminal_a.potential=*terminal_a.potential+charge_difference;
-    *terminal_b.potential=*terminal_b.potential-charge_difference;
-}
-
-void Capacitor::run_step(const double time){
-    double charge_difference((get_voltage()/resistance)*time);
-    *terminal_a.potential=*terminal_a.potential+charge_difference;
-    *terminal_b.potential=*terminal_b.potential-charge_difference;
+void Capacitor::run_step(const double time) {
+  double charge_difference((get_voltage() / resistance) * time);
+  *terminal_a.potential = *terminal_a.potential + charge_difference;
+  *terminal_b.potential = *terminal_b.potential - charge_difference;
 }

61387604a41cecc1271f753510f2caefbec138d6
Paul ALNET <alnet@insa-toulouse.fr>
2023-11-02 15:35:52 +0100

    fix(4-network): various in Network.cc

--- 4-simulator/Network.cc
+++ 4-simulator/Network.cc
@@ -7,12 +7,14 @@
 using namespace std;
 
 double const Component::get_voltage() {
-  return *terminal_b.potential - *terminal_a.potential;
+  return terminal_b.potential - terminal_a.potential;
 }
 
-std::string const Component::&get_name() { return &name; }
+std::string const &Component::get_name() { return name; }
 
-void add_component(Component &component) { push_back(component); };
+void Network::add_component(Component &component) {
+  components.push_back(component);
+};
 
 double const Component::get_current() { return 0; }
 
@@ -25,12 +27,12 @@ double const Capacitor::get_current() {
 void Capacitor::run_step(const double time) {
   double charge_difference(capacitance * get_voltage() * time);
   stored_charge = stored_charge + charge_difference;
-  *terminal_a.potential = *terminal_a.potential + charge_difference;
-  *terminal_b.potential = *terminal_b.potential - charge_difference;
+  terminal_a.potential = terminal_a.potential + charge_difference;
+  terminal_b.potential = terminal_b.potential - charge_difference;
 }
 
-void Capacitor::run_step(const double time) {
+void Resistor::run_step(const double time) {
   double charge_difference((get_voltage() / resistance) * time);
-  *terminal_a.potential = *terminal_a.potential + charge_difference;
-  *terminal_b.potential = *terminal_b.potential - charge_difference;
+  terminal_a.potential = terminal_a.potential + charge_difference;
+  terminal_b.potential = terminal_b.potential - charge_difference;
 }

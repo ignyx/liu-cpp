33258d15bd2d27065a336869700fb42fcd4c87cf
Paul ALNET <alnet@insa-toulouse.fr>
2023-11-02 15:47:45 +0100

    chore(4-network): group class methods

--- 4-simulator/Network.cc
+++ 4-simulator/Network.cc
@@ -7,6 +7,8 @@ double Component::get_voltage() {
   return terminal_b.potential - terminal_a.potential;
 }
 
+double Component::get_current() { return 0; }
+
 void Component::run_step(const double) {}
 
 std::string const &Component::get_name() { return name; }
@@ -15,9 +17,13 @@ void Network::add_component(Component &component) {
   components.push_back(component);
 }
 
-double Component::get_current() { return 0; }
-
 double Resistor::get_current() { return get_voltage() / resistance; }
+
+void Resistor::run_step(const double time) {
+  double charge_difference((get_voltage() / resistance) * time);
+  terminal_a.potential = terminal_a.potential + charge_difference;
+  terminal_b.potential = terminal_b.potential - charge_difference;
+}
 
 double Capacitor::get_current() {
   return capacitance * (get_voltage() - stored_charge);
@@ -26,12 +32,6 @@ double Capacitor::get_current() {
 void Capacitor::run_step(const double time) {
   double charge_difference(capacitance * get_voltage() * time);
   stored_charge = stored_charge + charge_difference;
-  terminal_a.potential = terminal_a.potential + charge_difference;
-  terminal_b.potential = terminal_b.potential - charge_difference;
-}
-
-void Resistor::run_step(const double time) {
-  double charge_difference((get_voltage() / resistance) * time);
   terminal_a.potential = terminal_a.potential + charge_difference;
   terminal_b.potential = terminal_b.potential - charge_difference;
 }

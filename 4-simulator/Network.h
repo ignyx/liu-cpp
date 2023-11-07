b1b741e4428144ef5beb0d12945c43b946bc887c
Paul ALNET <alnet@insa-toulouse.fr>
2023-11-07 14:25:49 +0100

    feat(4-network): make functions const

--- 4-simulator/Network.h
+++ 4-simulator/Network.h
@@ -14,10 +14,10 @@ public:
             Connection &terminal_b)
       : name{name}, terminal_a{terminal_a}, terminal_b{terminal_b} {};
   virtual ~Component() = default; // virtual destructor
-  double get_voltage();
-  virtual double get_current();
+  double get_voltage() const;
+  virtual double get_current() const;
   virtual void run_step(const double time);
-  std::string const &get_name();
+  std::string const &get_name() const;
 
 private:
   std::string name;
@@ -31,7 +31,7 @@ class Network {
 public:
   Network() : components{} {};
   void simulate(unsigned int interations, unsigned int lines, double time_step);
-  std::vector<Component> &get_components();
+  std::vector<Component> &get_components() const;
   void add_component(Component &component);
   // deallocate ?
 private:
@@ -55,7 +55,7 @@ public:
   Resistor(std::string const &name, double resistance, Connection terminal_a,
            Connection terminal_b)
       : Component{name, terminal_a, terminal_b}, resistance{resistance} {};
-  double get_current();
+  double get_current() const;
   void run_step(const double time);
 
 private:
@@ -68,7 +68,7 @@ public:
             Connection terminal_b)
       : Component{name, terminal_a, terminal_b}, capacitance{capacitance},
         stored_charge{0} {};
-  double get_current();
+  double get_current() const;
   void run_step(const double time);
 
 private:

528dc15e1e49bc056a7f912799c767897bd615f4
Paul ALNET <paul@shimaore.net>
2023-11-28 14:22:39 +0100

    chore(4-network): lint

--- 4-simulator/Network.cc
+++ 4-simulator/Network.cc
@@ -6,10 +6,9 @@
 
 using namespace std;
 
-
 Component::Component(std::string const &name, Connection &terminal_a,
-            Connection &terminal_b)
-      : name{name}, terminal_a{terminal_a}, terminal_b{terminal_b} {};
+                     Connection &terminal_b)
+    : name{name}, terminal_a{terminal_a}, terminal_b{terminal_b} {};
 
 double Component::get_voltage() const {
   return terminal_b.potential - terminal_a.potential;
@@ -61,9 +60,9 @@ void Network::simulate(unsigned int iterations, unsigned int lines,
   }
 }
 
-Resistor::Resistor(std::string const &name, double resistance, Connection &terminal_a,
-           Connection &terminal_b)
-      : Component{name, terminal_a, terminal_b}, resistance{resistance} {};
+Resistor::Resistor(std::string const &name, double resistance,
+                   Connection &terminal_a, Connection &terminal_b)
+    : Component{name, terminal_a, terminal_b}, resistance{resistance} {};
 
 double Resistor::get_current() const { return get_voltage() / resistance; }
 
@@ -73,10 +72,10 @@ void Resistor::run_step(const double time) {
   terminal_b.potential -= charge_difference;
 }
 
-Capacitor::Capacitor(std::string const &name, double capacitance, Connection &terminal_a,
-            Connection &terminal_b)
-      : Component{name, terminal_a, terminal_b}, capacitance{capacitance},
-        stored_charge{0} {};
+Capacitor::Capacitor(std::string const &name, double capacitance,
+                     Connection &terminal_a, Connection &terminal_b)
+    : Component{name, terminal_a, terminal_b}, capacitance{capacitance},
+      stored_charge{0} {};
 
 double Capacitor::get_current() const {
   return capacitance * (get_voltage() - stored_charge);
@@ -91,9 +90,9 @@ void Capacitor::run_step(const double time) {
   terminal_b.potential -= charge_difference;
 }
 
-Battery::Battery(std::string const &name, double voltage, Connection &terminal_a,
-          Connection &terminal_b)
-      : Component{name, terminal_a, terminal_b}, voltage{voltage} {};
+Battery::Battery(std::string const &name, double voltage,
+                 Connection &terminal_a, Connection &terminal_b)
+    : Component{name, terminal_a, terminal_b}, voltage{voltage} {};
 
 double Battery::get_current() const { return 0; }
 

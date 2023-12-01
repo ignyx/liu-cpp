3f2dbf53721b0be2a8c328da61b336ff745ae095
Paul ALNET <alnet@insa-toulouse.fr>
2023-12-01 11:23:35 +0100

    feat(5-network): main, read and print file

---
+++ 5-editor/main.cc
@@ -0,0 +1,53 @@
+#include <forward_list>
+#include <fstream>
+#include <iostream>
+#include <iterator>
+
+using namespace std;
+
+forward_list<string> read_words_from_file(ifstream &file);
+void print(forward_list<string> &words);
+void print_help(char *arg0);
+
+int main(int argc, char **argv) {
+  if (argc == 1) {
+    print_help(argv[0]);
+    exit(1);
+  }
+
+  char *file_name = argv[1];
+  ifstream file{file_name};
+  forward_list<string> text;
+  forward_list<string> arguments{argv + 2, argv + argc};
+
+  if (!file.is_open()) {
+    cout << "failed to open " << file_name << endl;
+    exit(1);
+  }
+
+  try {
+    text = read_words_from_file(file);
+  } catch (const ifstream::failure &error) {
+    cout << "failed to read from " << file_name << ": " << error.what() << endl;
+  }
+
+  print(text);
+  print(arguments);
+  return 0;
+}
+
+forward_list<string> read_words_from_file(ifstream &file) {
+  forward_list<string> words{istream_iterator<string>{file},
+                             istream_iterator<string>{}};
+  return words;
+}
+
+void print(forward_list<string> &words) {
+  copy(words.begin(), words.end(), ostream_iterator<string>{cout, " "});
+  cout << endl;
+}
+
+void print_help(char *arg0) {
+  cout << "usage: " << arg0 << " <file> [arguments]" << endl;
+  // TODO
+}

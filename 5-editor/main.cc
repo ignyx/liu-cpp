5c1685abef92f9df17649ec588bc7e0fd245e54e
Paul ALNET <paul@shimaore.net>
2023-12-02 15:02:53 +0100

    feat(5-network): add execute_argument

--- 5-editor/main.cc
+++ 5-editor/main.cc
@@ -10,6 +10,7 @@ using namespace std;
 forward_list<string> read_words_from_file(ifstream &file);
 void print(forward_list<string> &words);
 void print_help(char *arg0);
+void execute_argument(const string &argument, forward_list<string> &text);
 
 int main(int argc, char **argv) {
   if (argc == 1) {
@@ -33,8 +34,11 @@ int main(int argc, char **argv) {
     cout << "failed to read from " << file_name << ": " << error.what() << endl;
   }
 
-  print(text);
-  print(arguments);
+  for_each(arguments.begin(), arguments.end(), [&text](const string &argument) {
+    execute_argument(argument, text);
+  });
+  // print(text);
+  // print(arguments);
   return 0;
 }
 
@@ -58,6 +62,33 @@ unordered_map<string, int> count_words(forward_list<string> &text) {
   // unsorted key-value pairs (O(1) average; O(n) worst-case)
   unordered_map<string, int> word_count;
   for_each(text.begin(), text.end(),
-           [&word_count](string word) { word_count[word]++; });
+           [&word_count](const string &word) { word_count[word]++; });
   return word_count;
+}
+
+void execute_argument(const string &argument, forward_list<string> &text) {
+  const size_t separator{argument.find('=')};
+  const size_t split{separator == string::npos ? argument.size() : separator};
+  const string flag = argument.substr(0, split);
+  const string parameter = argument.substr(split);
+
+  if (flag == "--print") {
+    if (parameter.size() > 0)
+      cerr << "warning: --print takes no parameter" << endl;
+    print(text);
+  } else if (flag == "--frequency") {
+    if (parameter.size() > 0)
+      cerr << "warning: --frequency takes no parameter" << endl;
+
+  } else if (flag == "--table") {
+    if (parameter.size() > 0)
+      cerr << "warning: --table takes no parameter" << endl;
+
+  } else if (flag == "--substitute") {
+
+  } else if (flag == "--remove") {
+
+  } else {
+    cerr << "warning: unknown flag " << flag << endl;
+  }
 }
